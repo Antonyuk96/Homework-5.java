e39a87f8e56a0e296151016d8da7d2414ef080b7
Antonyuk96 <113938370+Antonyuk96@users.noreply.github.com>
2023-03-18 21:37:45 +0300

    Delete GroupSeveInterfaсe.java

--- GroupSeveInterfaсe.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public interface GroupSaveInterface {
-
-    public void saveGroup(Group group)throws IOException;
-
-    public Group loadGroup() throws IOException;
-
-}
